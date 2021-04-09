9f69c9f360b5e21e23e2b97e53f62c0494e17ae7
Kristijan Čeple <kristijan.ceple@fer.hr>
2021-04-09 16:44:36 +0200

    Added LED Controller. Currently adding UART Controller

---
+++ Lab2/drivers/led_axi_ip_v1_00_a/src/led_axi_ip.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          E:\FaksHub\2020_2021\2.Semester_2020_2021\LRI2\Labs\LRI2_Labs\Lab2/drivers/led_axi_ip_v1_00_a/src/led_axi_ip.c
+* Version:           1.00.a
+* Description:       led_axi_ip Driver Source File
+* Date:              Fri Apr 09 04:15:10 2021 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "led_axi_ip.h"
+
+/************************** Function Definitions ***************************/
+
---
+++ Lab2/drivers/uart_cntrl_v1_00_a/src/uart_cntrl.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          E:\FaksHub\2020_2021\2.Semester_2020_2021\LRI2\Labs\LRI2_Labs\Lab2/drivers/uart_cntrl_v1_00_a/src/uart_cntrl.c
+* Version:           1.00.a
+* Description:       uart_cntrl Driver Source File
+* Date:              Fri Apr 09 13:23:47 2021 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "uart_cntrl.h"
+
+/************************** Function Definitions ***************************/
+
