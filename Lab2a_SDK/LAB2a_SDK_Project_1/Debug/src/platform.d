796c897ab198422a7102ccc55a943921608257e0
Kristijan Čeple <kristijan.ceple@fer.hr>
2021-04-12 04:12:49 +0200

    Came to program FPGA part

---
+++ Lab2a_SDK/LAB2a_SDK_Project_1/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ ../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xparameters.h \
+ ../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xil_cache.h \
+ ../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/mb_interface.h \
+ ../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xbasic_types.h \
+ ../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xil_types.h \
+ ../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xparameters.h:
+
+../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xil_cache.h:
+
+../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/mb_interface.h:
+
+../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xbasic_types.h:
+
+../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xil_types.h:
+
+../../LAB2a_SDK_Project_1_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
