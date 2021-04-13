62700d1e732675e4d5879298323d6795f7dc40ec
Kristijan Čeple <kristijan.ceple@fer.hr>
2021-04-13 02:09:06 +0200

    trying to fix project

---
+++ Lab2a_SDK/Lab2a_SDK_proj1/Debug/src/helloworld.d
@@ -0,0 +1,34 @@
+src/helloworld.d: ../src/helloworld.c \
+ ../../my_bsp/microblaze_0/include/xgpio.h \
+ ../../my_bsp/microblaze_0/include/xil_types.h \
+ ../../my_bsp/microblaze_0/include/xil_assert.h \
+ ../../my_bsp/microblaze_0/include/xstatus.h \
+ ../../my_bsp/microblaze_0/include/xbasic_types.h \
+ ../../my_bsp/microblaze_0/include/xgpio_l.h \
+ ../../my_bsp/microblaze_0/include/xil_io.h \
+ ../../my_bsp/microblaze_0/include/mb_interface.h \
+ ../../my_bsp/microblaze_0/include/xil_printf.h \
+ ../../my_bsp/microblaze_0/include/xparameters.h \
+ ../../my_bsp/microblaze_0/include/led_axi_ip.h
+
+../../my_bsp/microblaze_0/include/xgpio.h:
+
+../../my_bsp/microblaze_0/include/xil_types.h:
+
+../../my_bsp/microblaze_0/include/xil_assert.h:
+
+../../my_bsp/microblaze_0/include/xstatus.h:
+
+../../my_bsp/microblaze_0/include/xbasic_types.h:
+
+../../my_bsp/microblaze_0/include/xgpio_l.h:
+
+../../my_bsp/microblaze_0/include/xil_io.h:
+
+../../my_bsp/microblaze_0/include/mb_interface.h:
+
+../../my_bsp/microblaze_0/include/xil_printf.h:
+
+../../my_bsp/microblaze_0/include/xparameters.h:
+
+../../my_bsp/microblaze_0/include/led_axi_ip.h:
---
+++ Lab2a_SDK/Lab2a_SDK_proj1/Debug/src/platform.d
@@ -0,0 +1,21 @@
+src/platform.d: ../src/platform.c \
+ ../../my_bsp/microblaze_0/include/xparameters.h \
+ ../../my_bsp/microblaze_0/include/xil_cache.h \
+ ../../my_bsp/microblaze_0/include/mb_interface.h \
+ ../../my_bsp/microblaze_0/include/xbasic_types.h \
+ ../../my_bsp/microblaze_0/include/xil_types.h \
+ ../../my_bsp/microblaze_0/include/xparameters.h ../src/platform_config.h
+
+../../my_bsp/microblaze_0/include/xparameters.h:
+
+../../my_bsp/microblaze_0/include/xil_cache.h:
+
+../../my_bsp/microblaze_0/include/mb_interface.h:
+
+../../my_bsp/microblaze_0/include/xbasic_types.h:
+
+../../my_bsp/microblaze_0/include/xil_types.h:
+
+../../my_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
