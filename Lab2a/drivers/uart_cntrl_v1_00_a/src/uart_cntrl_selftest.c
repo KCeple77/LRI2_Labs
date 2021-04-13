3636e2d3eba0243d73d90a5f9468544489688fa2
Kristijan Čeple <kristijan.ceple@fer.hr>
2021-04-13 03:07:59 +0200

    Successfully programmed the FPGA

--- Lab2a/drivers/uart_cntrl_v1_00_a/src/uart_cntrl_selftest.c
+++ Lab2a/drivers/uart_cntrl_v1_00_a/src/uart_cntrl_selftest.c
@@ -67,9 +67,9 @@ XStatus UART_CNTRL_SelfTest(void * baseaddr_p)
    */
   xil_printf("User logic slave module test...\n\r");
 
-  for (write_loop_index = 0 ; write_loop_index < UART_CNTRL_USER_NUM_REG; write_loop_index++)
+  for (write_loop_index = 0 ; write_loop_index < TEST_AXI_LITE_USER_NUM_REG; write_loop_index++)
     UART_CNTRL_mWriteSlaveReg0 (baseaddr, write_loop_index*4, (write_loop_index+1)*READ_WRITE_MUL_FACTOR);
-  for (read_loop_index = 0 ; read_loop_index < UART_CNTRL_USER_NUM_REG; read_loop_index++)
+  for (read_loop_index = 0 ; read_loop_index < TEST_AXI_LITE_USER_NUM_REG; read_loop_index++)
     if ( UART_CNTRL_mReadSlaveReg0 (baseaddr, read_loop_index*4) != (read_loop_index+1)*READ_WRITE_MUL_FACTOR){
       xil_printf ("Error reading register value at address %x", (int)baseaddr + read_loop_index*4);
       return XST_FAILURE;
